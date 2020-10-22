2a4dfe8e89fdd863b8e20a6b0ffe4e46c13d6cae
SpreeHertz <spreehertzofficial@gmail.com>
2020-10-22 15:24:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const queue = new Map();
 
 client.once('ready', () => {
     console.log('Troupes is online!');
-    client.user.setActivity('+about | V2.2.1 | Music command is being remade')
+    client.user.setActivity('+about | V2.2.1 |I now work 24/7! Bot is getting remade.')
     
     
     
