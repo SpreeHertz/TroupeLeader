d46ef5fa43b86258ab4e48c1ada42ede02fff3cb
SpreeHertz <spreehertzofficial@gmail.com>
2020-10-23 09:02:43 +0530

    summary is required.

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const queue = new Map();
 
 client.once('ready', () => {
     console.log('Troupes is online!');
-    client.user.setActivity('+about | V2.2.1 |I now work 24/7! Bot is getting remade.')
-    
+    client.user.setActivity('+about | V2.2.1 | I now work 24/7! Bot is getting remade.')
+    client.user.setStatus('dnd');
     
     
 });
@@ -19,11 +19,8 @@ client.once('ready', () => {
 
 
 
-client.on('message', msg => {
-    if (msg.author.bot) return undefined;
-    if(msg.content.startsWith(prefix)) return undefined;
 
-})
+
 
 
 
